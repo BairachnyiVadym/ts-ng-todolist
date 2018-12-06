f4a69dae688e0db5e8a8d9a047aefec4c867bee7
BairachnyiVadym <coworking1b.v@gmail.com>
2018-12-06 15:27:32 +0200

    Create local-storage service, add according imports, create two methods with store/remove functionality.

---
+++ src/app/storage/local-storage.service.ts
@@ -0,0 +1,30 @@
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'angular-webstorage-service';
+
+const STORAGE_KEY = 'local-todolist';
+
+@Injectable()
+export class LocalStorageService {
+
+  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
+
+  public storeOnLocalStorage(taskTitle = '', identifier): void {
+    if (taskTitle === undefined || taskTitle === '') { return; }
+
+    const currentTodoList = this.storage.get(STORAGE_KEY) || [];
+
+    currentTodoList.push({title: taskTitle, isDone: false, id: identifier});
+
+    this.storage.set(STORAGE_KEY, currentTodoList);
+  }
+
+  public removeFromLocalStorage(identifier): void {
+    const currentTodoList = this.storage.get(STORAGE_KEY);
+
+    const index = currentTodoList.findIndex(todo => todo.id === identifier);
+
+    currentTodoList.splice(index, 1);
+
+    this.storage.set(STORAGE_KEY, currentTodoList);
+  }
+}
